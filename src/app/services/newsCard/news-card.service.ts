f31b01d19017867a598ad53af076ad43ff25dfb9
riccardo mori <riccardomori@MacBook-Air-STOC.local>
2021-09-30 21:23:38 +0200

    premi settembre

--- src/app/services/newsCard/news-card.service.ts
+++ src/app/services/newsCard/news-card.service.ts
@@ -9,10 +9,27 @@ export class NewsCardService {
   constructor() { }
 
   getAllNews() : Card[] {
-    return [... this.news];
+    return [... this.news_2022];
   }
 
-  private news : Card[] = [
+  private news_2022 : Card[] = [
+    {
+      isChecked: false,
+      titolo: "Premi di Settembre",
+      contenuto: [
+        "Macinatore di classifica:  AC Denti",
+        "",
+        "Puntaiolo:  Trucidi",
+        "",
+        "Freno a mano:  Garantiti e DAMBROSIANI",
+        "",
+        "Benvenuti alla prima premiazione dell'anno. Ogni mese si eleggono le squadre che fanno più discutere l'intero mondo delle fantapioppe. Ogni mese si assegnano tre premi.",
+        "La squadra che si aggiudica il premio 'Macinatore di classifica' è la squadra che ha fatto più punti in classifica durante il mese.",
+        "Il secondo premio si intitola 'Puntaiolo' e lo riceve la squadra che ha fatto più punti durante i calcoli delle giornate del mese (somma dei punteggi di ogni partita del mese).",
+        "L'ultimo premio è il più ambito dagli atleti di cristo, poichè gli ultimi saranno i primi, il suo nome è 'Freno a mano' e lo si aggiudica la squadra che ha raccolto meno punti in classifica durante il mese"
+      ],
+      immagine: "./assets/img/news/crescita/crescitaACDenti.jpg"
+    },
     {
       isChecked: false,
       titolo: "Pazzesco Maignan il più pagato al mondo",
